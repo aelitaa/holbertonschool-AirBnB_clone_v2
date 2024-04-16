30014ca01be729e446dc04cce9445514b6b52984
aelitaa <gunel.sultanova@outlook.com>
2024-04-16 13:23:53 +0400

    changes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', strict_slashes=False)
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello HBNB!</p>"
+
+if __name__ = '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
