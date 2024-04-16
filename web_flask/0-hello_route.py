f6310a610f515b35d8d501797676fcb2e9d255c9
aelitaa <gunel.sultanova@outlook.com>
2024-04-16 13:38:53 +0400

    hello route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
+""" Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
+    """ Function that returns hello """
     return "<p>Hello HBNB!</p>"
 
 if __name__ = '__main__':
